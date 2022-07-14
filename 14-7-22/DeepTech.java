d75e10c60b2ba22baa71f619b44ddc6976d39e64
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-14 13:13:25 +0530

    Update DeepTech.java

--- 14-7-22/DeepTech.java
+++ 14-7-22/DeepTech.java
@@ -2,22 +2,27 @@ class DeepTech
 {
 public static void main(String arg[])
     {
-        int i;
-        for(i=1;i<=100;i++)
-        {
-          if(i%3==0 && i%5==0)
-            {
-              System.out.println("multiple by 3 Deep");
-            }
-          else if(i%3==0)
-            {
-              System.out.println("multiple by 5 Tech");
-            }
-          else if(i%5==0)
-            {
-              System.out.println("multiple by 3 & 5 DeepTech");
-            }
+		int i;
+		for(i=1;i<=100;i++)
+		{
+			  if(i%3==0 && i%5==0)
+				    {
+				      System.out.println("multiple by 3 Deep");
+				    }
+			  else if(i%3==0)
+				    {
+				      System.out.println("multiple by 5 Tech");
+				    }
+			  else if(i%5==0)
+				    {
+				      System.out.println("multiple by 3 & 5 DeepTech");
+				    }
+			  else
+				   {
+					System.out.println(i);
+				    }
 
-        }
-	  }
+		}
+
+	}
 }
