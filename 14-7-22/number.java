d8c51ae69094cd2ebdc263a9b9979f06d28cd2ed
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-14 12:28:58 +0530

    Create number.java

---
+++ 14-7-22/number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class number
+{
+public static void main(String arg[])
+	{ 
+			Scanner s=new Scanner(System.in);
+			System.out.println("enter the number");
+			int num=s.nextInt();
+			if(num%2==0)
+				{
+					System.out.println("number is positive");
+				}
+			else
+				{
+					System.out.println("number is negetive");
+				}
+	}
+}
