e0de3e5e5e52793905e2bd083e87afa427411981
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-14 12:21:27 +0530

    Create sumDigit.java

---
+++ 14-7-22/sumDigit.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class sumDigit
+{
+public static void main(String arg[])
+	{
+			Scanner s=new Scanner(System.in);
+			System.out.println("enter the number");
+			int num=s.nextInt();
+			int sum=0;
+			int a=num;
+			int rem=0;
+			while(a>0)
+				{
+					rem=a%10;
+					sum=sum+rem;
+					a/=10;
+				}
+			System.out.println("sum of digit:- "+sum);
+	}
+
+}		
