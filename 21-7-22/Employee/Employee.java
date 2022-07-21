5710e105350e85566094c432bfbcec51042ce5b6
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-21 18:06:30 +0530

    Create Employee.java

---
+++ 21-7-22/Employee/Employee.java
@@ -0,0 +1,32 @@
+
+public class Employee {
+	String EmpName;
+	int YearOfJoin;
+	int Salary;
+	public String getEmpName() {
+		return EmpName;
+	}
+	public void setEmpName(String empName) {
+		EmpName = empName;
+	}
+	public int getYearOfJoin() {
+		return YearOfJoin;
+	}
+	public void setYearOfJoin(int yearOfJoin) {
+		YearOfJoin = yearOfJoin;
+	}
+	public int getSalary() {
+		return Salary;
+	}
+	public void setSalary(int salary) {
+		Salary = salary;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	String Address;
+	
+}
