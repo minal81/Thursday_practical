a619e1caedc5c57a2d6e145a53df0b5f05701877
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-09-15 14:02:49 +0530

    Update ByteArrayOutputStreamEx.java

--- 15-9-22/ByteArrayOutputStream/ByteArrayOutputStreamEx.java
+++ 15-9-22/ByteArrayOutputStream/ByteArrayOutputStreamEx.java
@@ -1,35 +1,36 @@
 
-import java.io.*;
-class ByteArrayOutputStreamEX
-{
-    public static void main(String args[]) throws Exception
-    {
-        ByteArrayOutputStream f = new ByteArrayOutputStream(12);
-        System.out.println ("enter 5 characters and a return");
-        System.out.println ("These will be converted to uppercase and displayed");
-        while (f.size() != 5)
-        {
-            f.write(System.in.read( ) );
-        }
-        System.out.println("Accepted characters into an array");
-        byte b[] = f.toByteArray();
 
-        System.out.println("displaying characters in the array");
-        for(int l=0;l<b.length;l++)
-        {
-            System.out.println((char)b[l]);
-        }
-        ByteArrayInputStream inp = new ByteArrayInputStream(b);
-        int c;
-        System.out.println("converted to upper case characters");
-        for (int i=0;i<1;i++)
-        {
-            while((c=inp.read()) != -1)
-        {
-        System.out.print(Character.toUpperCase((char)c));
-        }
-        System.out.println();
-        inp.reset();
-        }
-    }
+import java.io.*;
+class ByteArrayOutputStreamEx
+{
+public static void main(String args[]) throws Exception
+{
+ByteArrayOutputStream f = new ByteArrayOutputStream(12);
+System.out.println ("enter 5 characters string");
+
+while (f.size() != 5)
+{
+f.write(System.in.read( ) );
+}
+
+byte b[] = f.toByteArray();
+System.out.println("convert into array");
+for(int l=0;l<b.length;l++)
+{
+System.out.println((char)b[l]);
+}
+
+ByteArrayInputStream inp = new ByteArrayInputStream(b);
+int c;
+System.out.println("converted to upper case characters");
+for (int i=0;i<1;i++)
+{
+while((c=inp.read()) != -1)
+{
+System.out.print(Character.toUpperCase((char)c));
+}
+System.out.println();
+inp.reset();
+}
+}
 }
